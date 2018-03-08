0a6b679ed93f751c6d413ed12dc368e65b01f354
Cavalier, Thomas <thomas.cavalier@pgi.com>
2018-03-08 10:42:24 -0700

    push to pi

--- binarylights.py
+++ binarylights.py
@@ -2,13 +2,6 @@ import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 import UTILS as UTILS
 from time import sleep # Import the sleep function from the time module
 
-def int2bin(i):
-	return list("{0:b}".format(i))
-
-def isOn(val):
-	return val == '1'
-
-
 pins = {16,18,22,32,36,31,33,37}
 
 GPIO.setwarnings(False) # Ignore warning for now
@@ -20,7 +13,7 @@ for pin in pins:
 n = 0
 while True: # Run forever
 	binaryNumbers = UTILS.int2bin(n)
-	for index, pin in pins:
+	for index, pin in enumerate(pins):
 		if UTILS.isOn(index):
 			GPIO.output(pin, GPIO.HIGH) # Turn on
 		else:
