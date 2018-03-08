0f98ca465481a593d2cad4345324e9957150c449
Cavalier, Thomas <thomas.cavalier@pgi.com>
2018-03-08 11:54:19 -0700

    push to pi

--- stoplight.py
+++ stoplight.py
@@ -1,46 +1,46 @@
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 
-def stop():
-	yellow();
-	GPIO.output(pin[0], GPIO.HIGH) # Turn on
-	GPIO.output(pin[1], GPIO.HIGH) # Turn on
-	GPIO.output(pin[2], GPIO.LOW) # Turn off
-	GPIO.output(pin[3], GPIO.LOW) # Turn off
-	GPIO.output(pin[4], GPIO.LOW) # Turn off
-	GPIO.output(pin[5], GPIO.LOW) # Turn off
-	GPIO.output(pin[6], GPIO.LOW) # Turn off
-	GPIO.output(pin[7], GPIO.LOW) # Turn off
+def stop(pins):
+	yellow(pins);
+	GPIO.output(pins[0], GPIO.HIGH) # Turn on
+	GPIO.output(pins[1], GPIO.HIGH) # Turn on
+	GPIO.output(pins[2], GPIO.LOW) # Turn off
+	GPIO.output(pins[3], GPIO.LOW) # Turn off
+	GPIO.output(pins[4], GPIO.LOW) # Turn off
+	GPIO.output(pins[5], GPIO.LOW) # Turn off
+	GPIO.output(pins[6], GPIO.LOW) # Turn off
+	GPIO.output(pins[7], GPIO.LOW) # Turn off
 
-def yellow():
-	GPIO.output(pin[0], GPIO.LOW) # Turn off
-	GPIO.output(pin[1], GPIO.LOW) # Turn off
-	GPIO.output(pin[2], GPIO.HIGH) # Turn on
-	GPIO.output(pin[3], GPIO.HIGH) # Turn on
-	GPIO.output(pin[4], GPIO.LOW) # Turn off
-	GPIO.output(pin[5], GPIO.LOW) # Turn off
-	GPIO.output(pin[6], GPIO.LOW) # Turn off
-	GPIO.output(pin[7], GPIO.LOW) # Turn off
+def yellow(pins):
+	GPIO.output(pins[0], GPIO.LOW) # Turn off
+	GPIO.output(pins[1], GPIO.LOW) # Turn off
+	GPIO.output(pins[2], GPIO.HIGH) # Turn on
+	GPIO.output(pins[3], GPIO.HIGH) # Turn on
+	GPIO.output(pins[4], GPIO.LOW) # Turn off
+	GPIO.output(pins[5], GPIO.LOW) # Turn off
+	GPIO.output(pins[6], GPIO.LOW) # Turn off
+	GPIO.output(pins[7], GPIO.LOW) # Turn off
 	sleep(5)
 
-def go():
-	GPIO.output(pin[0], GPIO.LOW) # Turn off
-	GPIO.output(pin[1], GPIO.LOW) # Turn off
-	GPIO.output(pin[2], GPIO.LOW) # Turn off
-	GPIO.output(pin[3], GPIO.LOW) # Turn on
-	GPIO.output(pin[4], GPIO.HIGH) # Turn on
-	GPIO.output(pin[5], GPIO.HIGH) # Turn on
-	GPIO.output(pin[6], GPIO.LOW) # Turn off
-	GPIO.output(pin[7], GPIO.LOW) # Turn off
+def go(pins):
+	GPIO.output(pins[0], GPIO.LOW) # Turn off
+	GPIO.output(pins[1], GPIO.LOW) # Turn off
+	GPIO.output(pins[2], GPIO.LOW) # Turn off
+	GPIO.output(pins[3], GPIO.LOW) # Turn on
+	GPIO.output(pins[4], GPIO.HIGH) # Turn on
+	GPIO.output(pins[5], GPIO.HIGH) # Turn on
+	GPIO.output(pins[6], GPIO.LOW) # Turn off
+	GPIO.output(pins[7], GPIO.LOW) # Turn off
 
-def walk():
-	GPIO.output(pin[0], GPIO.LOW) # Turn off
-	GPIO.output(pin[1], GPIO.LOW) # Turn off
-	GPIO.output(pin[2], GPIO.LOW) # Turn off
-	GPIO.output(pin[3], GPIO.LOW) # Turn on
-	GPIO.output(pin[4], GPIO.LOW) # Turn on
-	GPIO.output(pin[5], GPIO.LOW) # Turn on
-	GPIO.output(pin[6], GPIO.HIGH) # Turn off
-	GPIO.output(pin[7], GPIO.HIGH) # Turn off
+def walk(pins):
+	GPIO.output(pins[0], GPIO.LOW) # Turn off
+	GPIO.output(pins[1], GPIO.LOW) # Turn off
+	GPIO.output(pins[2], GPIO.LOW) # Turn off
+	GPIO.output(pins[3], GPIO.LOW) # Turn on
+	GPIO.output(pins[4], GPIO.LOW) # Turn on
+	GPIO.output(pins[5], GPIO.LOW) # Turn on
+	GPIO.output(pins[6], GPIO.HIGH) # Turn off
+	GPIO.output(pins[7], GPIO.HIGH) # Turn off
 
 pins = [16,18,22,32,36,31,33,37]
 
@@ -54,13 +54,13 @@ while True:
 	n = raw_input("Please enter 'hello':")
 	if n == 'stop':
 		print('light changing to yellow then red')
-		stop()
+		stop(pins)
 	elif n == 'go':
 		print('light changin to green')
-		go()
+		go(pins)
 	elif n == 'walk':
 		print('light changin to walk')
-		wlak()
+		wlak(pins)
 	elif n == 'q' or n == 'exit':
 		exit()
 	else:
