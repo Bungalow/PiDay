2be732a1b80e6593cbdcd57407b70fd74e31b824
Cavalier, Thomas <thomas.cavalier@pgi.com>
2018-03-08 13:54:43 -0700

    push to pi

---
+++ button.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+from time import sleep # Import the sleep function from the time module
+
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+
+GPIO.setup(17,GPIO.IN)
+GPIO.setup(27.GPIO.OUT)
+
+while True:
+	if GPIO.input(17) == False:
+		GPIO.output(27,GPIO.HIGH)
+	elif GPIO.input(17) == True:
+		GPIO.output(27,GPIO.LOW)
---
+++ resetClock.py
@@ -0,0 +1,11 @@
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+
+pins = [16,18,22,32,36,31,33,37]
+
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+
+for pin in pins:
+	GPIO.setup(pin, GPIO.OUT, initial=GPIO.LOW)
+
+GPIO.cleanup()
