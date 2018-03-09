5d9f375abb972cb2b4501b0123ae8eb7fbb31b4a
Cavalier, Thomas <thomas.cavalier@pgi.com>
2018-03-09 10:13:01 -0700

    push to pi

--- stoppableCounter.py
+++ stoppableCounter.py
@@ -3,10 +3,10 @@ import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 import UTILS as UTILS
 from time import sleep # Import the sleep function from the time module
 
-pins = [16,18,22,32,36,31,33,37]
-buttonPin = 11
-buttonLEDPin = 13
-buttonToggleState = True
+def pins = [16,18,22,32,36,31,33,37]
+def buttonPin = 11
+def buttonLEDPin = 13
+def buttonToggleState = True
 
 def incrementBinaryCounter(n):
 	n += 1
@@ -34,19 +34,22 @@ def setup():
 	GPIO.setup(buttonLEDPin, GPIO.OUT, initial=GPIO.LOW)
 	GPIO.output(buttonLEDPin, GPIO.LOW)
 
+def main():
+	n = 0
+	while True: # Run forever
+		checkForButtonPress()
+		if buttonToggleState:
+			binaryNumbers = UTILS.int2bin(n)
+			values = UTILS.getBinaryOnArray(binaryNumbers)
+			for index, ledOn in enumerate(values):
+				pin = pins[index];
+				if( ledOn ):
+					GPIO.output(pin, GPIO.HIGH) # Turn on
+				else:
+					GPIO.output(pin, GPIO.LOW) # Turn off
+			n = incrementBinaryCounter(n);
+			sleep(1)
+
 setup()
-n = 0
-while True: # Run forever
-	checkForButtonPress()
-	if buttonToggleState:
-		binaryNumbers = UTILS.int2bin(n)
-		values = UTILS.getBinaryOnArray(binaryNumbers)
-		for index, ledOn in enumerate(values):
-			pin = pins[index];
-			if( ledOn ):
-				GPIO.output(pin, GPIO.HIGH) # Turn on
-			else:
-				GPIO.output(pin, GPIO.LOW) # Turn off
-		n = incrementBinaryCounter(n);
-		sleep(1)
+main()
 GPIO.cleanup()
