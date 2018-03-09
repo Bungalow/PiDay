8d7c0896b3a33fd55ee0caf5392964203f1eee67
Cavalier, Thomas <thomas.cavalier@pgi.com>
2018-03-09 12:42:03 -0700

    push to pi

--- number.py
+++ number.py
@@ -4,13 +4,13 @@ import UTILS as UTILS
 from time import sleep # Import the sleep function from the time module
 
 def zero():
-	GPIO.output(A, GPIO.LOW) # Turn on
-	GPIO.output(F, GPIO.LOW) # Turn on
-	GPIO.output(E, GPIO.LOW) # Turn on
-	GPIO.output(D, GPIO.LOW) # Turn on
-	GPIO.output(C, GPIO.LOW) # Turn on
-	GPIO.output(G, GPIO.HIGH) # Turn off
-	GPIO.output(B, GPIO.LOW) # Turn on
+	GPIO.output(A, GPIO.HIGH) # Turn on
+	GPIO.output(F, GPIO.HIGH) # Turn on
+	GPIO.output(E, GPIO.HIGH) # Turn on
+	GPIO.output(D, GPIO.HIGH) # Turn on
+	GPIO.output(C, GPIO.HIGH) # Turn on
+	GPIO.output(G, GPIO.LOW) # Turn off
+	GPIO.output(B, GPIO.HIGH) # Turn on
 
 A = 16
 F = 18
@@ -23,13 +23,13 @@ B = 33
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 
-GPIO.setup(A, GPIO.OUT, initial=GPIO.HIGH)
-GPIO.setup(F, GPIO.OUT, initial=GPIO.HIGH)
-GPIO.setup(E, GPIO.OUT, initial=GPIO.HIGH)
-GPIO.setup(D, GPIO.OUT, initial=GPIO.HIGH)
-GPIO.setup(C, GPIO.OUT, initial=GPIO.HIGH)
-GPIO.setup(G, GPIO.OUT, initial=GPIO.HIGH)
-GPIO.setup(B, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(A, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(F, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(E, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(D, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(C, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(G, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(B, GPIO.OUT, initial=GPIO.LOW)
 
 while True:
 	zero()
