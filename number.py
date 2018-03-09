29a5a8d70b277c457b40fa7c5321f8ea21dda727
Cavalier, Thomas <thomas.cavalier@pgi.com>
2018-03-09 13:00:46 -0700

    push to pi

--- number.py
+++ number.py
@@ -22,76 +22,76 @@ def one():
 	GPIO.output(B, GPIO.LOW) # Turn on
 
 def two():
-	GPIO.output(A, GPIO.HIGH) # Turn off
+	GPIO.output(A, GPIO.HIGH) # Turn on
 	GPIO.output(F, GPIO.LOW) # Turn off
-	GPIO.output(E, GPIO.HIGH) # Turn off
-	GPIO.output(D, GPIO.HIGH) # Turn off
+	GPIO.output(E, GPIO.HIGH) # Turn on
+	GPIO.output(D, GPIO.HIGH) # Turn on
 	GPIO.output(C, GPIO.LOW) # Turn off
-	GPIO.output(G, GPIO.HIGH) # Turn off
-	GPIO.output(B, GPIO.HIGH) # Turn off
+	GPIO.output(G, GPIO.HIGH) # Turn on
+	GPIO.output(B, GPIO.HIGH) # Turn on
 
 def three():
-	GPIO.output(A, GPIO.LOW) # Turn off
+	GPIO.output(A, GPIO.HIGH) # Turn on
 	GPIO.output(F, GPIO.LOW) # Turn off
 	GPIO.output(E, GPIO.LOW) # Turn off
-	GPIO.output(D, GPIO.LOW) # Turn off
-	GPIO.output(C, GPIO.LOW) # Turn off
-	GPIO.output(G, GPIO.LOW) # Turn off
-	GPIO.output(B, GPIO.LOW) # Turn off
+	GPIO.output(D, GPIO.HIGH) # Turn on
+	GPIO.output(C, GPIO.HIGH) # Turn on
+	GPIO.output(G, GPIO.HIGH) # Turn on
+	GPIO.output(B, GPIO.HIGH) # Turn on
 
 def four():
 	GPIO.output(A, GPIO.LOW) # Turn off
-	GPIO.output(F, GPIO.LOW) # Turn off
+	GPIO.output(F, GPIO.HIGH) # Turn on
 	GPIO.output(E, GPIO.LOW) # Turn off
 	GPIO.output(D, GPIO.LOW) # Turn off
-	GPIO.output(C, GPIO.LOW) # Turn off
-	GPIO.output(G, GPIO.LOW) # Turn off
-	GPIO.output(B, GPIO.LOW) # Turn off
+	GPIO.output(C, GPIO.HIGH) # Turn on
+	GPIO.output(G, GPIO.HIGH) # Turn on
+	GPIO.output(B, GPIO.HIGH) # Turn on
 
 def five():
-	GPIO.output(A, GPIO.LOW) # Turn off
-	GPIO.output(F, GPIO.LOW) # Turn off
+	GPIO.output(A, GPIO.HIGH) # Turn on
+	GPIO.output(F, GPIO.HIGH) # Turn on
 	GPIO.output(E, GPIO.LOW) # Turn off
-	GPIO.output(D, GPIO.LOW) # Turn off
-	GPIO.output(C, GPIO.LOW) # Turn off
-	GPIO.output(G, GPIO.LOW) # Turn off
+	GPIO.output(D, GPIO.HIGH) # Turn on
+	GPIO.output(C, GPIO.HIGH) # Turn on
+	GPIO.output(G, GPIO.HIGH) # Turn on
 	GPIO.output(B, GPIO.LOW) # Turn off
 
 def six():
 	GPIO.output(A, GPIO.LOW) # Turn off
-	GPIO.output(F, GPIO.LOW) # Turn off
-	GPIO.output(E, GPIO.LOW) # Turn off
-	GPIO.output(D, GPIO.LOW) # Turn off
-	GPIO.output(C, GPIO.LOW) # Turn off
-	GPIO.output(G, GPIO.LOW) # Turn off
+	GPIO.output(F, GPIO.HIGH) # Turn on
+	GPIO.output(E, GPIO.HIGH) # Turn on
+	GPIO.output(D, GPIO.HIGH) # Turn on
+	GPIO.output(C, GPIO.HIGH) # Turn on
+	GPIO.output(G, GPIO.HIGH) # Turn on
 	GPIO.output(B, GPIO.LOW) # Turn off
 
 def seven():
-	GPIO.output(A, GPIO.LOW) # Turn off
+	GPIO.output(A, GPIO.HIGH) # Turn on
 	GPIO.output(F, GPIO.LOW) # Turn off
 	GPIO.output(E, GPIO.LOW) # Turn off
 	GPIO.output(D, GPIO.LOW) # Turn off
-	GPIO.output(C, GPIO.LOW) # Turn off
+	GPIO.output(C, GPIO.HIGH) # Turn on
 	GPIO.output(G, GPIO.LOW) # Turn off
-	GPIO.output(B, GPIO.LOW) # Turn off
+	GPIO.output(B, GPIO.HIGH) # Turn on
 
 def eight():
-	GPIO.output(A, GPIO.LOW) # Turn off
-	GPIO.output(F, GPIO.LOW) # Turn off
-	GPIO.output(E, GPIO.LOW) # Turn off
-	GPIO.output(D, GPIO.LOW) # Turn off
-	GPIO.output(C, GPIO.LOW) # Turn off
-	GPIO.output(G, GPIO.LOW) # Turn off
-	GPIO.output(B, GPIO.LOW) # Turn off
+	GPIO.output(A, GPIO.HIGH) # Turn on
+	GPIO.output(F, GPIO.HIGH) # Turn on
+	GPIO.output(E, GPIO.HIGH) # Turn on
+	GPIO.output(D, GPIO.HIGH) # Turn on
+	GPIO.output(C, GPIO.HIGH) # Turn on
+	GPIO.output(G, GPIO.HIGH) # Turn on
+	GPIO.output(B, GPIO.HIGH) # Turn on
 
 def nine():
-	GPIO.output(A, GPIO.LOW) # Turn off
-	GPIO.output(F, GPIO.LOW) # Turn off
+	GPIO.output(A, GPIO.HIGH) # Turn on
+	GPIO.output(F, GPIO.HIGH) # Turn on
 	GPIO.output(E, GPIO.LOW) # Turn off
 	GPIO.output(D, GPIO.LOW) # Turn off
-	GPIO.output(C, GPIO.LOW) # Turn off
-	GPIO.output(G, GPIO.LOW) # Turn off
-	GPIO.output(B, GPIO.LOW) # Turn off
+	GPIO.output(C, GPIO.HIGH) # Turn on
+	GPIO.output(G, GPIO.HIGH) # Turn on
+	GPIO.output(B, GPIO.HIGH) # Turn on
 
 A = 16
 F = 18
@@ -117,3 +117,33 @@ while True:
 	sleep(1)
 	one()
 	sleep(1)
+	two()
+	sleep(1)
+	three()
+	sleep(1)
+	four()
+	sleep(1)
+	five()
+	sleep(1)
+	six()
+	sleep(1)
+	seven()
+	sleep(1)
+	eight()
+	sleep(1)
+	nine()
+	sleep(1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
