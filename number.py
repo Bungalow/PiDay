5d8859c65e34e3d0f6f053bffa9b7df85d3cf0aa
Cavalier, Thomas <thomas.cavalier@pgi.com>
2018-03-09 11:44:11 -0700

    push to pi

--- number.py
+++ number.py
@@ -3,10 +3,34 @@ import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 import UTILS as UTILS
 from time import sleep # Import the sleep function from the time module
 
-pins = [16,18,22,32,36,31,33]
+def zero(pins){
+	GPIO.output(A, GPIO.LOW) # Turn off
+	GPIO.output(F, GPIO.LOW) # Turn off
+	GPIO.output(E, GPIO.LOW) # Turn off
+	GPIO.output(D, GPIO.LOW) # Turn off
+	GPIO.output(C, GPIO.LOW) # Turn off
+	GPIO.output(G, GPIO.HIGH) # Turn off
+	GPIO.output(B, GPIO.LOW) # Turn off
+}
+
+A = 16
+F = 18
+E = 22
+D = 32
+C = 36
+G = 31
+B = 33
 
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 
-for pin in pins:
-	GPIO.setup(pin, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(A, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(F, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(E, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(D, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(C, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(G, GPIO.OUT, initial=GPIO.HIGH)
+GPIO.setup(B, GPIO.OUT, initial=GPIO.HIGH)
+
+while True:
+	zero()
